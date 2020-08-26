ed7e21eecd971f6f7cb3200da6fd820f50b2d5d0
maple3142 <kirby741852963@gmail.com>
2020-08-27 01:15:52 +0800

    add color to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+WHITE='\033[1;37m'
+LGREEN='\033[1;32m'
+CYAN='\033[0;36m'
+LOG_PREFIX="${LGREEN}Setup: ${WHILE}"
+log(){
+        printf "${LOG_PREFIX}${CYAN}$1${WHITE}\n"
+}
 cd ~
-echo "Removing .oh-my-zsh .temphome..."
+log "Removing .oh-my-zsh .temphome..."
 rm -rf .oh-my-zsh .temphome
-echo "Installing oh my zsh..."
+log "Installing oh my zsh..."
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-echo "Cloning dotfiles to .temphome..."
+log "Cloning dotfiles to .temphome..."
 git clone https://github.com/maple3142/dotfiles.git .temphome
 rm -rf .temphome/.git .temphome/setup.sh
-echo "Copying dotfiles to home..."
+log "Copying dotfiles to home..."
 cp -a -rf -- .temphome/. .
-echo "Removing .temphome..."
+log "Removing .temphome..."
 rm -rf .temphome
-echo "Cloning extra zsh plugins..."
+log "Cloning extra zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-echo "Changing shell to zsh..."
+log "Changing shell to zsh..."
 sudo chsh -s $(which zsh)
 
