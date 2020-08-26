322e1782378dc7c502e9c2a68415b9f5c0555b01
maple3142 <kirby741852963@gmail.com>
2020-08-26 22:55:18 +0800

    remove .temphome

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd ~
-rm -rf .oh-my-zsh
+rm -rf .oh-my-zsh .temphome
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 git clone https://github.com/maple3142/dotfiles.git .temphome
 rm -rf .temphome/.git .temphome/setup.sh
 cp -a -rf -- .temphome/. .
+rm -rf .temphome
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
