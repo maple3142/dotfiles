461276f7e5d4fdf530594117a32dee5ed9205a27
maple3142 <kirby741852963@gmail.com>
2020-09-20 18:22:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,19 +15,15 @@ require(){
 require zsh || exit $?
 require git || exit $?
 cd ~ || { echo "Unable to cd ~"; exit 1; }
-log "Removing .oh-my-zsh .temphome..."
-rm -rf .oh-my-zsh .temphome
-log "Installing oh my zsh..."
-curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 log "Cloning dotfiles to .temphome..."
 git clone https://github.com/maple3142/dotfiles.git .temphome
 log "Copying dotfiles to home..."
-cp -a -rf -- .temphome/{.zshrc,.vimrc,.prettierrc} .
+cp -a -rf -- .temphome/{.zshrc,.p10k.zsh,.vimrc,.prettierrc} .
 log "Removing .temphome..."
 rm -rf .temphome
-log "Cloning extra zsh plugins..."
-git clone https://github.com/zsh-users/zsh-autosuggestions.git .oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git .oh-my-zsh/custom/plugins/fast-syntax-highlighting
+log "Installing zinit..."
+mkdir .zinit
+git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
 log "Changing shell to zsh..."
 chsh -s "$(which zsh)"
 log "All done! Restart your terminal or enter \`zsh\` to enjoy!"
